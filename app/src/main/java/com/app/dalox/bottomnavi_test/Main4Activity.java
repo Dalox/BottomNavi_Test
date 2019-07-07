9e56573b9728effb5b3bb85cf5bbc02dcd8c10c8
Dalox <jdn95@gmail.com>
2019-07-07 18:14:19 -0500

    Initial commit

---
+++ app/src/main/java/com/app/dalox/bottomnavi_test/Main4Activity.java
@@ -0,0 +1,13 @@
+package com.app.dalox.bottomnavi_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
